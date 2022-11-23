215f29bb71da9a6c7dda405892250b3a8d1faa20
eq4606 <eq4606@gmail.com>
2022-11-23 22:31:56 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProcessComponent } from './process/process.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'process', component: ProcessComponent },
+  { path: '**', redirectTo: 'process', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProcessComponent } from './process/process.component';
+import { SumProcessComponent } from './sum-process/sum-process.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { SumContactsComponent } from './sum-contacts/sum-contacts.component';
+import { PhoneNumberPipe } from './phone-number.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProcessComponent,
+    SumProcessComponent,
+    ContactsComponent,
+    SumContactsComponent,
+    PhoneNumberPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
